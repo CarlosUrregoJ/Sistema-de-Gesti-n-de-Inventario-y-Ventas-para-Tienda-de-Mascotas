57d1b27df59a74f6a2d838ac7fcb82e4da10e8fe
Carlos Santiago Urrego Jiménez <urregojcs@gmail.com>
2024-10-17 16:02:49 -0500

    Primer avance proyecto venta productos veterinaria

---
+++ ventaproductosmascotas/src/main/java/co/ucentral/ventaproductosmascotas/VentaproductosmascotasApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.ventaproductosmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaproductosmascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaproductosmascotasApplication.class, args);
+	}
+
+}
---
+++ ventaproductosmascotas/src/test/java/co/ucentral/ventaproductosmascotas/VentaproductosmascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.ventaproductosmascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaproductosmascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
